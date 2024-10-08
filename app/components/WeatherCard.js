dbc15e8ef57e353339e25b15fd563c53662cfbc6
Nidhi Desu <nd2290@nyu.edu>
2024-10-08 09:33:14 -0400

    adding icons

--- app/components/WeatherCard.js
+++ app/components/WeatherCard.js
@@ -1,23 +1,33 @@
 import styles from "../page.module.css"
+import WeatherIcon from "./WeatherIcon"
+// import cloud from "@/app/weatherapp-cloud.png"
 
-export default function WeatherCard({ city, currentTemp, highTemp, lowTemp, cloudiness, humidity, windspeed }) {
+export default function WeatherCard({ city, currentTemp, highTemp, lowTemp, cloudiness, humidity, windspeed, weatherType }) {
     return (
         <div className={styles.weatherCardWrapper}>
             <div className={styles.cardTop}>
                 <h2>{city}</h2>
             </div>
             <div className={styles.cardBottom}>
-                <div className={styles.cardBottomLeft}>x</div>
+                <div className={styles.cardBottomLeft}>
+                    <WeatherIcon weatherType={weatherType}/>
+                    {/* <img src="@/app/weatherapp-cloud.png" alt="cloud"/> */}
+                </div>
                 <div className={styles.cardBottomRight}>
                     <div className={styles.mainDetails}>
                         <p className={styles.currTemp}>{currentTemp}°F</p>
-                        <p>Cloudiness: {cloudiness}</p>
+                        <p>Cloudiness: {cloudiness}%</p>
                     </div>
                     <div className={styles.minorDetails}>
-                        <p>High Temperature: {highTemp}°F</p>
-                        <p>Humidity: {humidity}%</p>
-                        <p>Low Temperature: {lowTemp}°F</p>
-                        <p>Wind Speed: {windspeed} MPH</p>
+                        <div>
+                            <p>High: {highTemp}°F</p>
+                            <p>Low: {lowTemp}°F</p>
+                        </div>
+                        <div>
+                            <p>Humidity: {humidity}%</p>
+                            <p>Wind Speed: {windspeed} MPH</p>
+                        </div>
+                        
                     </div>
                 </div>
             </div>
---
+++ app/components/WeatherIcon.js
@@ -0,0 +1,13 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUmbrella, faCloud, faSun, faSmog, faSnowflake } from '@fortawesome/free-solid-svg-icons'
+
+export default function WeatherIcon({weatherType}) {
+    switch (weatherType) {
+        case "Clouds": return <FontAwesomeIcon icon={faCloud} />;
+        case "Rain": return <FontAwesomeIcon icon={faUmbrella} />;
+        case "Clear": return <FontAwesomeIcon icon={faSun} />;
+        case "Mist": return <FontAwesomeIcon icon={faSmog} />;
+        case "Snow": return <FontAwesomeIcon icon={faSnowflake} />;
+        default: return <FontAwesomeIcon icon={faSun} />;
+    }
+}
